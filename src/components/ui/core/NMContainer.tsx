2a25ea548dd4d40be4e9067f807fbdc98472aa8e
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-01 09:11:59 +0600

    create reusable NMContainer

---
+++ src/components/ui/core/NMContainer.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react';
+interface INMContainerProps {
+	children:ReactNode,
+	className:string
+}
+const NMContainer = ({children, className}: INMContainerProps) => {
+	return (
+		<div className={`container mx-auto px-5 ${className}`}>
+			{children}
+		</div>
+	);
+};
+
+export default NMContainer;
