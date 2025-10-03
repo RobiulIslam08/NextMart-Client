c004653bbe5b848778078fcc6723e39959949dba
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-03 07:44:46 +0600

    Setting Up the Store and Hooks

---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+
+//! We will not do this
+//! This is a global variable so we will avoid this
+// const store = configureStore({});
+export const makeStore = () => {
+  return configureStore({
+    reducer: {},
+  })
+}
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
