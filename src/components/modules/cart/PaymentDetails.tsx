d5445162f661b3c533e661edf146f96777a935f5
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-03 15:56:46 +0600

    Calculating the Subtotal Automatically

--- src/components/modules/cart/PaymentDetails.tsx
+++ src/components/modules/cart/PaymentDetails.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { subTotalSelector } from "@/redux/features/cartSlice";
+import { useAppSelector } from "@/redux/hooks";
 // import { useUser } from "@/context/UserContext";
 // import { currencyFormatter } from "@/lib/currencyFormatter";
 // import {
@@ -19,7 +21,7 @@ import { Button } from "@/components/ui/button";
 // import { toast } from "sonner";
 
 export default function PaymentDetails() {
-//   const subTotal = useAppSelector(subTotalSelector);
+  const subTotal = useAppSelector(subTotalSelector);
 //   const shippingCost = useAppSelector(shippingCostSelector);
 //   const grandTotal = useAppSelector(grandTotalSelector);
 //   const order = useAppSelector(orderSelector);
@@ -74,7 +76,7 @@ export default function PaymentDetails() {
       <div className="space-y-2 mt-4">
         <div className="flex justify-between">
           <p className="text-gray-500 ">Subtotal</p>
-          <p className="font-semibold">200</p>
+          <p className="font-semibold">{subTotal}</p>
         </div>
         <div className="flex justify-between">
           <p className="text-gray-500 ">Discount</p>
--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -3,52 +3,72 @@ import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
 export interface CartProduct extends IProduct {
-   orderQuantity:number
+  orderQuantity: number;
 }
 interface InitialState {
-	products:CartProduct[]
+  products: CartProduct[];
 }
-const initialState:InitialState = {
-	products:[]
-}
+const initialState: InitialState = {
+  products: [],
+};
 const cartSlice = createSlice({
-	name:"cart",
-	initialState,
-	reducers:{
-	 addProduct:(state, action) => {
-		const productToAdd = state.products.find((product) => product._id === action.payload._id)
-		if(productToAdd){
-			productToAdd.orderQuantity += 1;
-			return;
-		}
+  name: "cart",
+  initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      const productToAdd = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+      if (productToAdd) {
+        productToAdd.orderQuantity += 1;
+        return;
+      }
 
-		state.products.push({...action.payload, orderQuantity:1})
-
-	 },
-	 incrementOrderQuantity: (state, action) => { //action.payload এ _id আসবে। 
-		const productToIncrement = state.products.find((product) => product._id === action.payload)
-		if(productToIncrement){
-			productToIncrement.orderQuantity +=1;
-			return;
-		}
-	 },
-	 decrementOrderQuantity: (state, action) => { //action.payload এ _id আসবে। 
-		const productToIncrement = state.products.find((product) => product._id === action.payload)
-		if(productToIncrement && productToIncrement.orderQuantity >1){
-			productToIncrement.orderQuantity -=1;
-			return;
-		}
-	 },
-	    removeProduct: (state, action) => {
+      state.products.push({ ...action.payload, orderQuantity: 1 });
+    },
+    incrementOrderQuantity: (state, action) => {
+      //action.payload এ _id আসবে।
+      const productToIncrement = state.products.find(
+        (product) => product._id === action.payload
+      );
+      if (productToIncrement) {
+        productToIncrement.orderQuantity += 1;
+        return;
+      }
+    },
+    decrementOrderQuantity: (state, action) => {
+      //action.payload এ _id আসবে।
+      const productToIncrement = state.products.find(
+        (product) => product._id === action.payload
+      );
+      if (productToIncrement && productToIncrement.orderQuantity > 1) {
+        productToIncrement.orderQuantity -= 1;
+        return;
+      }
+    },
+    removeProduct: (state, action) => {
       state.products = state.products.filter(
         (product) => product._id !== action.payload
       );
     },
-
-	}
-})
-export const orderedProductsSelector = (state:RootState) => {
-	return state.cart.products
-}
-export const {addProduct,incrementOrderQuantity, decrementOrderQuantity, removeProduct} = cartSlice.actions
+  },
+});
+export const orderedProductsSelector = (state: RootState) => {
+  return state.cart.products;
+};
+export const subTotalSelector = (state: RootState) => {
+  return state.cart.products.reduce((acc, product) => {
+    if (product.offerPrice) {
+      return acc + product.offerPrice * product.orderQuantity;
+    } else {
+      return acc + product.price * product.orderQuantity;
+    }
+  }, 0);
+};
+export const {
+  addProduct,
+  incrementOrderQuantity,
+  decrementOrderQuantity,
+  removeProduct,
+} = cartSlice.actions;
 export default cartSlice.reducer;
