cce08fbb09b84f68e0a787a88f53a00dc7addd83
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-10-03 09:02:45 +0600

    create cart reducer

---
+++ src/redux/features/cartSlice.ts
@@ -0,0 +1,15 @@
+import { IProduct } from "@/types";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface InitialState {
+	products:IProduct[]
+}
+const initialState:InitialState = {
+	products:[]
+}
+const cartSlice = createSlice({
+	name:"cart",
+	initialState,
+	reducers:{}
+})
+export default cartSlice.reducer;
